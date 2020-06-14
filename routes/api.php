85f8cedc7d93d4b9b696f64288946e427ce86b3e
Figo <figoaranta@gmail.com>
2020-06-14 22:54:05 +0700

    test session

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::prefix('v1')->group(function(){
 
 	Route::delete('/deleteCartAll/{id}/{accountId}','Api\v1\CartController@deleteCartItemAll');
 	Route::delete('/deleteCart/{id}/{accountId}', 'Api\v1\CartController@deleteCartItem');
-	Route::post('/addToCart/{productId}/{accountId}','Api\v1\CartController@testCart');
+	Route::post('/addToCart/{productId}/{accountId}','Api\v1\CartController@addToCart');
 	Route::get('/cart/{id}', 'Api\v1\CartController@viewCart');
 });
 
