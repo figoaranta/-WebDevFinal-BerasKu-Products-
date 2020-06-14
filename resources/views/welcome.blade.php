21734203901856b4dc02ba756992cd03e2448e9b
Figo <figoaranta@gmail.com>
2020-06-14 23:56:13 +0700

    test session

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,5 @@
-<form action = "https://berasku-products.herokuapp.com/api/v1/addToCart/1/1" method="POST"> 
+<form action = "https:berasku.herokuapp.com/api/v1/addToCart/1/1" method="POST"> 
     <button type="submit" value="submit">Buy now</button>
 </form>
+
+<a href="https:berasku.herokuapp.com/api/v1/cart/1">cart</a>
